0881473b81a3cae6d43f56fcb251e590b9fd88a8
Juan Marchetto <juanpatriciomarchetto@gmail.com>
2020-04-02 19:43:27 -0300

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-## MemoFran
+## Memo game
+
+in order to make it work you should edit de elements array in src/elements.js file with paths to the images that you whant to include in the game
+
+DEMO:https://memofran.netlify.com/ 
--- src/helpers.js
+++ src/helpers.js
@@ -4,33 +4,18 @@ Given a unidimentional array and optional parameters
 (like criteria, or shape, in order to stablish the way that elements will be arrange in a bidimentional way)
 default behaviour will be a convertion to square if posible or a rectangule-like figure in a mobile first approach
 */
-export class linearToMatricial {
-  converter = (unidimentionalArray, params) => {
-    let bidimentionalArray;
-    if ((params = {})) {
-      bidimentionalArray = this.toGrid(unidimentionalArray);
-    }
-    return bidimentionalArray;
-  };
-}
 
 export function shuffle(array) {
   var currentIndex = array.length,
     temporaryValue,
     randomIndex;
-
-  // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-    // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-
-    // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
   return array;
 }
 
